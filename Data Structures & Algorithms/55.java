c9235130e99e440647ce434cccfddcdccfe32eec
Saloni Sharma <salonipadma2020@gmail.com>
2026-07-13 22:48:01 +0530

    Create 55.java

---
+++ Data Structures & Algorithms/55.java
@@ -0,0 +1,17 @@
+//Jump Game
+
+class Solution {
+    public boolean canJump(int[] nums) {
+   
+   int temp=nums.length-1;
+
+   for(int i=nums.length-2;i>=0;i--)
+   {
+    if(i+nums[i]>=temp)
+    {
+        temp=i;
+    }
+   }
+   return temp==0;
+}
+}
